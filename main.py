e8eca477397a770f1b35be92422ffb80e79375d3
Connor Clancy <clancy.mu@gmail.com>
2023-10-29 20:35:58 -0400

    initial example

---
+++ main.py
@@ -0,0 +1,35 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+# Create API Key env variable
+from secrets_store import api_key_openai
+import os 
+os.environ['OPENAI_API_KEY'] = api_key_openai
+
+chat = ChatOpenAI(temperature=0.9)
+
+template = (
+    "You are a helpful assistant that translates {input_language} to {output_language}."
+)
+system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+human_template = "{text}"
+human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+chat_prompt = ChatPromptTemplate.from_messages(
+    [system_message_prompt, human_message_prompt]
+)
+
+# get a chat completion from the formatted messages
+chat(
+    chat_prompt.format_prompt(
+        input_language="English", output_language="French", text="I love programming."
+    ).to_messages()
+)
+
+print(AIMessage(content="J'adore la programmation.", additional_kwargs={}, example=False))
